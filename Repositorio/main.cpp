33179a323768a944a687401da370b236b2cc2182
Cristianojr9 <42578466+Cristianojr9@users.noreply.github.com>
2018-11-08 10:52:35 -0200

    upp

--- Repositorio/main.cpp
+++ Repositorio/main.cpp
@@ -1,18 +1,41 @@
 #include <iostream>
 using namespace std;
 
-struct Pessoa{
+struct Professor{
     string nome;
-    int idade;
-    Pessoa(string nome = "", int idade = 0):
-        nome(nome), idade(idade){
+    int id;
+public:
+    Pessoa(string nome = "", int id = 0):
+        nome(nome), id(id){
     }
     string toString(){
-        return "[" + nome + ":" + to_string(idade) + "]";
+        return " " + id + ":" + to_string(nome);
     }
 };
 
 struct Disciplina{
+    string id;
+    string nome;
+public:
+    Disciplina(string id = "01", string nome = "aaa"):
+    id(id), nome(nome){
+    }
+    string toString(){
+        return " " + id + ":" + to_string(nome);
+    }
+};
+
+struct Aluno{
+    string id;
+    string curso;
+    string nome;
+public:
+    Aluno(string id = "", string curso = "", string nome = ""):
+        id(id), curso(curso), nome(nome){
+    }
+    string toString(){
+        return " " + id + ":" + curso ":" + to_string(nome);
+    }
 };
 
 struct Repositorio{
