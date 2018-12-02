a31cf61a50250084c016b1b42a15a0deedf82b61
Cristianojr9 <42578466+Cristianojr9@users.noreply.github.com>
2018-12-01 23:43:35 -0300

    up funcionando

--- Salario/main.cpp
+++ Salario/main.cpp
@@ -7,119 +7,119 @@
 using namespace std;
 
 class Funcionario{
-    string nome;
-    int qtd_diaria = 0, max_diaria;
-	double bonus = 0;
+protected:
+    string name, profissao;
+    int qtd_diaria = 0, max_diaria, bonus = 0, salario;
 public:
-    Funcionario(string nome):
-        nome(nome)
+    Funcionario(string name):
+        name(name)
 	{
 	}
-	virtual string getName() = 0;
+
+    virtual string getName() = 0;
+    virtual string getProfiss() = 0;
     virtual int getDiaria() = 0;
     virtual int getMaxDiaria() = 0;
+    virtual void calcSalario() = 0;
     virtual string toString() = 0;
-    virtual void setBonus(double val){
+    virtual void setBonus(int val){
 		bonus = val;
 	}
     virtual void addDiaria(){
-		this->qtd_diaria += 1;
+		this->qtd_diaria += 1;	
 	}
 };
 
 class Professor : public Funcionario{
-    int maxdiaria = {2};
+    string profissao = {"Professor"};
+    int max_diaria = {2};
     char classe;
 public:
-    Professor(string nm, char cs):
-        Funcionario(nm), classe(cs)
-	{
-	}
-	virtual string getName(){
-		return nome;
-	} 
-    virtual string getProfissao(){
-		return profissao;
-	}
-    virtual int getDiaria(){
-		return diaria;
-	}
-    virtual int getMaxDiaria(){
-		return maxdiaria;
-	}        
-
-    void calcSalario(){
-        switch(classe){
-            case 'A': salario = 3000; 
-				break;
-            case 'B': salario = 5000; 
-				break;
-            case 'C': salario = 7000; 
-				break;
-            case 'D': salario = 9000; 
-				break;
-            case 'E': salario = 11000; 
-				break;
-        }
-        salario += 100 * diaria + bonus;
-    }
-    string toString(){
-        stringstream ss;
-        ss << profissao + " " + nome + " classe " + classe + "\n" << "  salario " + to_string(salario);
-        return ss.str();
-    }
-};
-
-class SerTecAdm : public Funcionario{
-	string nome;
-    int maxdiaria = {1}, nivel;
-public:
-    SerTecAdm(string nome, int nivel):
-        nome(nome), nivel(nivel)
+    Professor(string name, char classe):
+        Funcionario(name), classe(classe)
 	{
 	}
 
     virtual string getName(){
-		return nome;
-	} 
-    virtual int getDiaria(){
-		return diaria;
-	}
-    virtual int getMaxDiaria(){
-		return maxdiaria;
-	}
-
-    void calcSalario(){
-        salario = 3000 + 300 * nivel + 100 * diaria + bonus;
-    }
-    string toString(){
-        stringstream ss;
-        ss << profissao + " " + nome + " nivel " + to_string(nivel) + "\n"
-        << "  salario " + to_string(salario);
-        return ss.str();
-    }
-};
-
-class Terceirizado : public Funcionario{
-    string profissao;
-    int maxdiaria = {-1}, horastrab;
-    string adicsalub;
-public:
-    Terceirizado(string nm, int ht, string as):
-        Funcionario(nm), horastrab(ht), adicsalub(as)
-	{
-	}
-	virtual string getName(){
-		return nome;
+		return name;
 	} 
     virtual string getProfiss(){
 		return profissao;
 	}
     virtual int getDiaria(){
-		return diaria;
+		return qtd_diaria;
 	}
     virtual int getMaxDiaria(){
-		return maxdiaria;
+		return max_diaria;
+	}        
+
+    void calcSalario(){
+        switch(classe){
+            case 'A': salario = 3000; break;
+            case 'B': salario = 5000; break;
+            case 'C': salario = 7000; break;
+            case 'D': salario = 9000; break;
+            case 'E': salario = 11000; break;
+        }
+        salario += 100 * qtd_diaria + bonus;
+    }
+    string toString(){
+        stringstream ss;
+        ss << profissao + " " + name + " classe " + classe + "\n"
+        << "  salario " + to_string(salario);
+        return ss.str();
+    }
+};
+
+class SerTecAdm : public Funcionario{
+    string profissao = {"Fulano"};
+    int max_diaria = {1}, nivel;
+public:
+    SerTecAdm(string name, int nivel):
+        Funcionario(name), nivel(nivel){}
+
+    virtual string getName(){
+		return name;
+	} 
+    virtual string getProfiss(){
+		return profissao;
+	}
+    virtual int getDiaria(){
+		return qtd_diaria;
+	}
+    virtual int getMaxDiaria(){
+		return max_diaria;
+	}
+
+    void calcSalario(){
+        salario = 3000 + 300 * nivel + 100 * qtd_diaria + bonus;
+    }
+    string toString(){
+        stringstream ss;
+        ss << profissao + " " + name + " nivel " + to_string(nivel) + "\n" << "  salario " + to_string(salario);
+        return ss.str();
+    }
+};
+
+class Terceirizado : public Funcionario{
+    string profissao = {"Tercerizado"};
+    int max_diaria = {-1}, horastrab;
+    string adicsalub;
+public:
+    Terceirizado(string name, int ht, string as):
+        Funcionario(name), horastrab(ht), adicsalub(as){}
+
+    virtual string getName(){
+		return name;
+	} 
+    virtual string getProfiss(){
+		return profissao;
+	}
+    virtual int getDiaria(){
+		return qtd_diaria;
+	}
+    virtual int getMaxDiaria(){
+		return max_diaria;
 	}
 
     void calcSalario(){
@@ -132,8 +132,7 @@ public:
         string adicional = "salubre";
         if(adicsalub == "sim")
             adicional = "insalubre";    
-        ss << profissao + " " + nome + " " + to_string(horastrab) + "h "
-        << adicional  + "\n" + "  salario " + to_string(salario);        
+        ss << profissao + " " + name + " " + to_string(horastrab) + "h " << adicional  + "\n" + "  salario " + to_string(salario);        
         return ss.str();
     }
 };
@@ -195,7 +194,7 @@ public:
 };
 
 class Controller{
-    Sistema<Funcionario> mySis;
+    Sistema<Funcionario> Sist;
 public:
     string shell(string line){
         stringstream in(line);
@@ -210,7 +209,7 @@ public:
                 char level;
                 in >> name >> level;
                 Professor* P = new Professor(name, level);
-                if(!mySis.addUser(name, P))
+                if(!Sist.addUser(name, P))
                     delete P;
                 out << "done";
             }
@@ -219,7 +218,7 @@ public:
                 int level;
                 in >> name >> level;
                 SerTecAdm* S = new SerTecAdm(name, level);
-                if(!mySis.addUser(name, S))
+                if(!Sist.addUser(name, S))
                     delete S;
                 out << "done";
             }
@@ -228,40 +227,39 @@ public:
                 int hours_work;
                 in >> name >> hours_work >> salub;
                 Terceirizado * T = new Terceirizado(name, hours_work, salub);
-                if(!mySis.addUser(name, T))
+                if(!Sist.addUser(name, T))
                     delete T;
                 out << "done";
             }
             else if(op == "rm"){
                 string name;
                 in >> name;
-                mySis.rmUser(name);
+                Sist.rmUser(name);
             }
             else if(op == "show"){
                 if(in >> op){
-                    Funcionario* f = mySis.getUser(op);
-                    mySis.refreshSalario(f);
-                    out << mySis.getUser(op)->toString();
+                    Funcionario* f = Sist.getUser(op);
+                    Sist.refreshSalario(f);
+                    out << Sist.getUser(op)->toString();
                 } else
-                    out << mySis.toString();
+                    out << Sist.toString();
             }
             else if(op == "addDiaria"){
                 string name;
                 in >> name;
-                mySis.addDiaria(name);
+                Sist.addDiaria(name);
                 out << "done";
             }
             else if(op == "setBonus"){
                 int bonus;
                 in >> bonus;
-                mySis.setBonus(bonus);
+                Sist.setBonus(bonus);
                 out << "done";
             }
         }
-        catch(char const* e){out << e;}
-        catch(string e){out << e;}
-        catch(...){out << "fail: ocorreu uma exceção";}
-        
+        catch(char const* e){
+			out << e;
+		}
         return out.str();
     }
 
